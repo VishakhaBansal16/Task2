7365843d877248988151332dc88335c0a6efb73d
"Vishakha Bansal" <"vishakhabansal16@gmail.com">
2022-05-27 19:29:36 +0530

    Initial commit

---
+++ personalToken.sol
@@ -0,0 +1,32 @@
+///SPDX-License-Identifier: Unlicense
+pragma solidity 0.8.4;
+
+import "@openzeppelin/contracts/token/ERC20/ERC20.sol";
+contract personalToken is ERC20{
+    address private tokenOwner = msg.sender;
+    address public tokenAddress;
+    constructor() ERC20("Token","tk"){       
+        _mint(tokenOwner, 1000);
+        tokenAddress=address(this);
+    }       
+}
+contract exchange{
+    IERC20 public tokenContract;
+    address owner;
+    uint256 private _price = 1;
+    constructor(address _tokenAddress,address _tokenOwner){
+        tokenContract = IERC20(_tokenAddress);
+        owner = _tokenOwner;
+    }
+    function purchaseToken() external payable {
+        require(msg.value>0,"Insufficient Balance");
+        tokenContract.transferFrom(owner,msg.sender,(msg.value)/_price);
+        payable(owner).transfer((msg.value)/_price);
+    }
+}
+contract rewardToken{
+
+}
+contract stake{
+
+}
